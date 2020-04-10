c2e2ba8d300badcb5c70f45282730dd4fe585633
wenglei96@163.com <wenglei96@163.com>
2020-04-10 13:11:17 +0800

    测试一下

--- src/main/java/com/wenglei/demo/demo01.java
+++ src/main/java/com/wenglei/demo/demo01.java
@@ -3,5 +3,6 @@ package com.wenglei.demo;
 public class demo01 {
     public static void main(String[] args) {
         System.out.println("测试使用");
+        System.out.println("测试更新");
     }
 }
---
+++ src/main/java/com/wenglei/demo/demo02.java
@@ -0,0 +1,7 @@
+package com.wenglei.demo;
+
+public class demo02 {
+    public static void main(String[] args) {
+        System.out.println("添加测试");
+    }
+}
